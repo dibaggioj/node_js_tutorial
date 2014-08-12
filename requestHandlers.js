74727b8d8d366d758f2e0aaac88cf4681e63c9be
John DiBaggio <jsantoku@gmail.com>
2014-08-12 10:27:51 -0500

    Handling POST requests
    1. defined that we expect the encoding of the received data to be UTF-8
    2. added an event listener for the “data” event which step by step
    fills our new postData variable whenever a new chunk of POST data
    arrives
    3. moved the call to our router into the end event callback to make
    sure it’s only called when all POST data is gathered
    4. pass the POST data into the router, because we are going to need it
    in our request handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,11 +5,24 @@ var exec = require("child_process").exec; // non-blocking operation: exec()
 function start(response) {
 	console.log("Request handler 'start' was called.");
 
-	exec("ls -lah", function (error, stdout, stderr) { // a more expensive operation than "ls -lah" (which gets a list of all files in the current directory) is "find /"
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(stdout);
-		response.end();
-	});
+	var body = '<html>'+
+		'<head>'+
+		'<meta http-equiv="Content-Type" content="text/html; '+
+		'charset=UTF-8" />'+
+		'</head>'+
+		'<body>'+
+		'<form action="/upload" method="post">'+
+		'<textarea name="text" rows="20" cols="60"></textarea>'+
+		'<input type="submit" value="Submit text" />'+
+		'</form>'+
+		'</body>'+
+		'</html>';
+
+	// exec("ls -lah", function (error, stdout, stderr) { // a more expensive operation than "ls -lah" (which gets a list of all files in the current directory) is "find /"
+	 	response.writeHead(200, {"Content-Type": "text/html"});
+	 	response.write(body);
+	 	response.end();
+	// });
 }
 
 function upload(response) {
--- server.js
+++ server.js
@@ -5,10 +5,23 @@ var url = require("url");
 //put server code into a function named 'start' and then export this function
 function start(route, handle) { // We’ve added the handle parameter to our start() function, and pass the handle object on to the route() callback, as its first parameter.
   function onRequest(request, response) {
+  	var postData = "";
 	var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname, response); // Instead of expecting a return value from the route() function, we pass it a third parameter, our 'response' object
+    request.setEncoding("utf8");
+
+	request.addListener("data", function(postDataChunk) { 
+		// called when a new chunk of data was received
+		postData += postDataChunk;
+		console.log("Received POST data chunk '"+
+		postDataChunk + "'.");
+	});
+
+    request.addListener("end", function() {
+    	// called when all chunks of data have been received
+    	route(handle, pathname, response, postData); // Instead of expecting a return value from the route() function, we pass it a third parameter, our 'response' object
+    });
 
   }
 
