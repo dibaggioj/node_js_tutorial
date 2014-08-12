31baee2cf3501552a2e47b2a436ce7789e346d4a
John DiBaggio <jsantoku@gmail.com>
2014-08-12 13:37:19 -0500

    handling file uploads
    1. integrate node-formidable into 'upload' request handler
    2. embed uploaded image into the HTML output of the '/upload' URL

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
 var querystring = require("querystring"),
-	fs = require("fs");
+	fs = require("fs"),
+	formidable = require("formidable");
 
 // Our handler functions need to accept the response parameter, and have to make use of them in order to respond to the request directly
 function start(response, postData) {
@@ -11,7 +12,7 @@ function start(response, postData) {
 		'</head>'+
 		'<body>'+
 		'<form action="/upload" enctype="multipart/form-data" method="post">'+
-		'<input type="file" value="upload" />'+
+		'<input type="file" name="upload" multiple="multiple">'+
 		'<input type="submit" value="Upload file" />'+
 		'</form>'+
 		'</body>'+
@@ -22,18 +23,33 @@ function start(response, postData) {
 	 	response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent: "+ 
-		querystring.parse(postData).text);
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done");
+
+		/* Possible error on Windows systems:
+			tried to rename to an already existing file */
+		fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+			if (error) {
+				fs.unlink("/tmp/test.png");
+				fs.rename(files.upload.path, "/tmp/test.png");
+			}
+		});
+	})
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("received image:<br/>");
+	response.write("<img src='/show' />"); 
 	response.end();
 }
 
 function show(response) {
 	console.log("Request handler 'show' was called.");
 	response.writeHead(200, {"Content-Type": "image/png"});
-	fs.createReadStream("tmp/test.png").pipe(response);
+	fs.createReadStream("/tmp/test.png").pipe(response);
 }
 
 exports.start = start;
